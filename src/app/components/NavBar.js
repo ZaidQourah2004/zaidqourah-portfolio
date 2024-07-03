a1505cda80b7726a6e6cb8b9ac16384ae2d3de87
ZaidQourah2004 <qourahzaid04@gmail.com>
2024-07-03 18:36:53 +0300

    Completed Navigaton Bar
    Completed Navigaton Bar.
    
    Added Personal Logo to NavBar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import Image from 'next/image';
 
 export default function Navbar() {
     return (
-        <nav className="relative flex justify-between items-center p-4 bg-gradient-to-r from-gray-900 via-indigo-800 to-gray-800 shadow-md">
+        <nav className="flex justify-between items-center p-4 bg-gradient-to-r from-gray-900 via-indigo-800 to-gray-800 shadow-md">
+            <div className="flex items-center">
+                <Link href="/" passHref>
+                    <div className="relative" style={{ width: '68px', height: '68px' }}> {/* Adjust the width and height as needed */}
+                        <Image src="/images/logo1.svg" alt="Logo" layout="fill" objectFit="contain" />
+                    </div>
+                </Link>
+            </div>
             <div className="flex-1 flex justify-center space-x-4">
                 <Link href="/" passHref>
                     <button className="text-gray-300 hover:text-indigo-400 px-3 py-2 border-b-2 border-transparent hover:border-indigo-400 transition">Home</button>
