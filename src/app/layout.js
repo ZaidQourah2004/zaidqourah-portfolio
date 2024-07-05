c2b53c4b5b0d9658aca95c1d077509d229e682ff
ZaidQourah2004 <qourahzaid04@gmail.com>
2024-07-05 14:45:14 +0300

    Update Layout.js
    Small changes to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
+import '../styles/animations.css';
 
 export default function RootLayout({ children }) {
     return (
         <html>
         <head>
-            <title>Zaid Qourah Portfolio</title>
+            <title>Zaid Qourah</title>
             <meta name="description" content="Zaid Qourah's personal portfolio website" />
             <link rel="icon" href="/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lora:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         </head>
         <body>
         {children}
