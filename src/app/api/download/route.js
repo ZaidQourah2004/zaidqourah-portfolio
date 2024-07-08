3d839daeff6153a5c7890c7564f70a49b7ae575e
ZaidQourah2004 <qourahzaid04@gmail.com>
2024-07-09 00:49:42 +0300

    Created route.js
    created route.js to handle the process of downloading a folder within a repo in github.
    
    allows flexibility to change the name of the downloaded file so that it can be different than what it appears on Github

---
+++ src/app/api/download/route.js
@@ -0,0 +1,63 @@
+import JSZip from 'jszip';
+import fetch from 'node-fetch';
+import { NextResponse } from 'next/server';
+
+const fetchJson = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+    }
+    return response.json();
+};
+
+const fetchFileContent = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+    }
+    return Buffer.from(await response.arrayBuffer());
+};
+
+const addFilesToZip = async (zip, files, basePath, newFolderName) => {
+    for (const file of files) {
+        const filePath = `${newFolderName}/${basePath}/${file.name}`;
+        if (file.type === 'file') {
+            const content = await fetchFileContent(file.download_url);
+            zip.file(filePath, content);
+        } else if (file.type === 'dir') {
+            const dirFiles = await fetchJson(file.url);
+            const folder = zip.folder(filePath);
+            await addFilesToZip(folder, dirFiles, `${basePath}/${file.name}`, newFolderName);
+        }
+    }
+};
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const owner = searchParams.get('owner');
+    const repo = searchParams.get('repo');
+    const path = searchParams.get('path');
+    const newFolderName = searchParams.get('newFolderName') || 'renamed-folder';
+    const branch = searchParams.get('branch') || 'main';
+    const encodedPath = encodeURIComponent(path);
+
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${encodedPath}?ref=${branch}`;
+
+    try {
+        const files = await fetchJson(apiUrl);
+        const zip = new JSZip();
+        await addFilesToZip(zip, files, '', newFolderName);
+
+        const content = await zip.generateAsync({ type: 'nodebuffer' });
+        const response = new NextResponse(content, {
+            headers: {
+                'Content-Disposition': `attachment; filename=${newFolderName}.zip`,
+                'Content-Type': 'application/zip'
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error('Error creating zip file:', error);
+        return NextResponse.json({ error: 'Error creating zip file.' }, { status: 500 });
+    }
+}
